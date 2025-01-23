c3428d27cdffc9a3e4c42f0585c259831f3d125f
Szilard <szilardigaz@protonmail.com>
2025-01-23 21:48:52 +0000

    added crafting recipes, ability to smelt ore and ability to mine blocks with different pickaxes.

--- src/main/java/com/szilard/shlurdmod/block/ModBlocks.java
+++ src/main/java/com/szilard/shlurdmod/block/ModBlocks.java
@@ -1,16 +1,16 @@
 package com.szilard.shlurdmod.block;
 
 import com.szilard.shlurdmod.Shlurdmod;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
@@ -19,6 +19,14 @@ public class ModBlocks {
 
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(4f).requiresTool()));
+
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -33,9 +41,5 @@ public class ModBlocks {
     public static void registerModBlocks() {
         Shlurdmod.LOGGER.info("Registering Mod Blocks for :" + Shlurdmod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-            entries.add(ModBlocks.PINK_GARNET_BLOCK);
-            entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
-        });
     }
 }
